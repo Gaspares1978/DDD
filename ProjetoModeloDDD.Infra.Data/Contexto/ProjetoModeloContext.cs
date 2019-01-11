9d1203640422673b434a27c04170e3a71437a6b3
gaspar <gaspares1978@gmail.com>
2019-01-11 15:54:24 -0200

    DDD

---
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoDDD.Domain.Entities;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace ProjetoModeloDDD1.Infra.Data.Contexto
+{
+    public class ProjetoModeloContext : DbContext
+
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+       
+
+
+
+
+        public ProjetoModeloContext() : base("ProjetoModeloDDD")
+        {
+                
+        }
+         
+
+        
+    }
+
+
+}
+
+
---
+++ ProjetoModeloDDD.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoModeloDDD1.MVC.MvcApplication" Language="C#" %>
